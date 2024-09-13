e283d4e4f551afaf037374d4f6316d90a306a7e0
Wolfgang Schütz <wolfgang.schuetz@anton-paar.com>
2024-09-13 10:36:48 +0200

    added tests

--- ex-math/statistics/inc/exmath-statistics.hpp
+++ ex-math/statistics/inc/exmath-statistics.hpp
@@ -13,31 +13,56 @@ namespace exmath::statistics
   class max_min_mean_value_calculator_t
   {
   public:
+    using count_type = std::size_t;
     using value_type = double;
 
     max_min_mean_value_calculator_t() = default;
 
-    max_min_mean_value_calculator_t(std::span<value_type> const values) { this->operator()(values); }
+    max_min_mean_value_calculator_t(value_type const & value) { this->operator()(value); }
 
-    template <typename functor>
-    max_min_mean_value_calculator_t(functor const& ftor, uint32_t max_idx)
-        : m_count(max_idx)
+    max_min_mean_value_calculator_t(std::span<value_type const> const& values)
     {
-      for (std::size_t i = 0; i < max_idx; i++)
+      for (value_type const value : values)
       {
-        value_type const val = ftor(i);
+        if (std::isnan(value))
+          continue;
 
-        if (this->m_max < val)
-          this->m_max = val;
-        if (this->m_min > val)
-          this->m_min = val;
+        if (this->m_max < value)
+          this->m_max = value;
+        if (this->m_min > value)
+          this->m_min = value;
 
-        this->m_mean += val;
-        this->m_x_sqr += val * val;
+        this->m_mean += value;
+        this->m_x_sqr += value * value;
       }
 
-      this->m_mean /= this->m_count;
-      this->m_x_sqr /= this->m_count;
+      this->m_count          = values.size();
+      value_type const scale = 1.0 / static_cast<value_type>(this->m_count);
+      this->m_mean *= scale;
+      this->m_x_sqr *= scale;
+    }
+
+    template <typename functor> max_min_mean_value_calculator_t(functor const& ftor, count_type max_idx)
+    {
+      for (count_type i = 0; i < max_idx; i++)
+      {
+        value_type const value = ftor(i);
+        if (std::isnan(value))
+          continue;
+
+        if (this->m_max < value)
+          this->m_max = value;
+        if (this->m_min > value)
+          this->m_min = value;
+
+        this->m_mean += value;
+        this->m_x_sqr += value * value;
+      }
+
+      this->m_count          = max_idx;
+      value_type const scale = 1.0 / static_cast<value_type>(this->m_count);
+      this->m_mean *= scale;
+      this->m_x_sqr *= scale;
     }
 
     auto operator()(value_type const& value) noexcept -> max_min_mean_value_calculator_t&
@@ -50,7 +75,9 @@ namespace exmath::statistics
       if (this->m_min > value)
         this->m_min = value;
 
-      this->m_count++;
+      if (this->m_count < std::numeric_limits<count_type>::max())
+        this->m_count++;
+
       value_type const scale = 1.0 / static_cast<value_type>(this->m_count);
       this->m_mean += (value - this->m_mean) * scale;
       this->m_x_sqr += (value * value - this->m_x_sqr) * scale;
@@ -58,13 +85,13 @@ namespace exmath::statistics
       return *this;
     }
 
-    auto operator()(std::span<value_type> const values) noexcept -> max_min_mean_value_calculator_t&
+    template <typename functor> auto operator()(functor const& ftor, count_type max_idx) noexcept -> max_min_mean_value_calculator_t&
     {
-      for (value_type const val : values)
-      {
-        this->operator()(val);
-      }
-      return *this;
+      return this->operator()(max_min_mean_value_calculator_t{ ftor, max_idx });
+    }
+    auto operator()(std::span<value_type const> const& values) noexcept -> max_min_mean_value_calculator_t&
+    {
+      return this->operator()(max_min_mean_value_calculator_t{ values });
     }
 
     auto operator()(max_min_mean_value_calculator_t const& value) noexcept -> max_min_mean_value_calculator_t&
@@ -74,33 +101,49 @@ namespace exmath::statistics
       if (this->m_min > value.m_min)
         this->m_min = value.m_min;
 
-      this->m_count += value.m_count;
+      if ((std::numeric_limits<count_type>::max() - this->m_count) > value.m_count)
+        this->m_count += value.m_count;
+      else
+        this->m_count = std::numeric_limits<count_type>::max();
+
       value_type const scale = static_cast<value_type>(value.m_count) / static_cast<value_type>(this->m_count);
       this->m_mean += (value.m_mean - this->m_mean) * scale;
       this->m_x_sqr += (value.m_x_sqr - this->m_x_sqr) * scale;
       return *this;
     }
 
-    value_type get_mean() const noexcept { return this->m_mean; }
-
+    count_type get_number_of_values() const noexcept { return this->m_count; }
     value_type get_max() const noexcept { return this->m_max; }
-
     value_type get_min() const noexcept { return this->m_min; }
-
-    value_type get_var() const noexcept
+    value_type get_mean() const noexcept
     {
-      value_type const scale = static_cast<value_type>(this->m_count) / static_cast<value_type>(this->m_count - 1);
-      return (this->m_x_sqr - this->m_mean * this->m_mean) * scale;
+      if (this->m_count == 0)
+        return std::numeric_limits<value_type>::quiet_NaN();
+      return this->m_mean;
     }
+    value_type get_variance() const noexcept
+    {
+      if (this->m_count < 2)
+        return std::numeric_limits<value_type>::quiet_NaN();
+      value_type const scale = static_cast<value_type>(this->m_count) / static_cast<value_type>(this->m_count - 1);
+      return std::abs(this->m_x_sqr - this->m_mean * this->m_mean) * scale;
+    }
+    value_type get_standard_deviation() const noexcept { return std::sqrt(this->get_variance()); }
 
   private:
-    uint32_t   m_count = 0;
+    count_type m_count = 0;
     value_type m_max   = -std::numeric_limits<value_type>::infinity();
     value_type m_min   = std::numeric_limits<value_type>::infinity();
     value_type m_mean  = 0.0f;
     value_type m_x_sqr = 0.0f;
   };
 
+  max_min_mean_value_calculator_t operator+(max_min_mean_value_calculator_t const& lhs, max_min_mean_value_calculator_t const& rhs) noexcept
+  {
+    max_min_mean_value_calculator_t ret{ lhs };
+    return ret(rhs);
+  }
+  max_min_mean_value_calculator_t& operator+=(max_min_mean_value_calculator_t& lhs, max_min_mean_value_calculator_t const& rhs) noexcept { return lhs(rhs); }
 }    // namespace exmath::statistics
 
 #endif
--- tst/statistics/tst_exmath-statistics.cpp
+++ tst/statistics/tst_exmath-statistics.cpp
@@ -1,3 +1,5 @@
+#include "testdata_max_min_mean_var_std.hpp"
+
 #include <array>
 #include <atomic>
 #include <exmath.hpp>
@@ -15,38 +17,37 @@ TEST_CASE()
   std::span<double> sp_2{ &values[3], 3 };
   std::span<double> sp_3{ &values[6], 3 };
 
-
   T blk_1{};
 
   blk_1(values[0]);
   REQUIRE(blk_1.get_max() == Approx(0));
   REQUIRE(blk_1.get_min() == Approx(0));
   REQUIRE(blk_1.get_mean() == Approx(0));
-  REQUIRE(std::isnan(blk_1.get_var()));
+  REQUIRE(std::isnan(blk_1.get_variance()));
 
   blk_1(values[1]);
   REQUIRE(blk_1.get_max() == Approx(1));
   REQUIRE(blk_1.get_min() == Approx(0));
   REQUIRE(blk_1.get_mean() == Approx(0.5));
-  REQUIRE(blk_1.get_var() == Approx(0.5));
+  REQUIRE(blk_1.get_variance() == Approx(0.5));
 
   blk_1(values[2]);
   REQUIRE(blk_1.get_max() == Approx(2));
   REQUIRE(blk_1.get_min() == Approx(0));
   REQUIRE(blk_1.get_mean() == Approx(1));
-  REQUIRE(blk_1.get_var() == Approx(1));
+  REQUIRE(blk_1.get_variance() == Approx(1));
 
   blk_1(values[3]);
   REQUIRE(blk_1.get_max() == Approx(3));
   REQUIRE(blk_1.get_min() == Approx(0));
   REQUIRE(blk_1.get_mean() == Approx(1.5));
-  REQUIRE(blk_1.get_var() == Approx(5.0 / 3.0));
+  REQUIRE(blk_1.get_variance() == Approx(5.0 / 3.0));
 
   blk_1(values[4]);
   REQUIRE(blk_1.get_max() == Approx(4));
   REQUIRE(blk_1.get_min() == Approx(0));
   REQUIRE(blk_1.get_mean() == Approx(2));
-  REQUIRE(blk_1.get_var() == Approx(2.5));
+  REQUIRE(blk_1.get_variance() == Approx(2.5));
 
   T blk_2{};
   blk_2(std::span<double>{ &values[5], 5 });
@@ -54,20 +55,210 @@ TEST_CASE()
   REQUIRE(blk_2.get_max() == Approx(9));
   REQUIRE(blk_2.get_min() == Approx(5));
   REQUIRE(blk_2.get_mean() == Approx(7));
-  REQUIRE(blk_2.get_var() == Approx(2.5));
+  REQUIRE(blk_2.get_variance() == Approx(2.5));
 
   blk_1(blk_2);
   REQUIRE(blk_1.get_max() == Approx(9));
   REQUIRE(blk_1.get_min() == Approx(0));
   REQUIRE(blk_1.get_mean() == Approx(4.5));
-  REQUIRE(blk_1.get_var() == Approx(9.0+5.0/30.0));
+  REQUIRE(blk_1.get_variance() == Approx(9.0 + 5.0 / 30.0));
 
   T blk_3{ sp_1 };
   REQUIRE(blk_3.get_max() == Approx(2));
   REQUIRE(blk_3.get_min() == Approx(0));
   REQUIRE(blk_3.get_mean() == Approx(1));
-  REQUIRE(blk_3.get_var() == Approx(1));
+  REQUIRE(blk_3.get_variance() == Approx(1));
+}
 
-  blk_1 + blk_2 blk_1(blk_2)
+TEST_CASE()
+{
+  using T = exmath::statistics::max_min_mean_value_calculator_t;
 
+  double values[10] = {
+    0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0,
+  };
+  std::span<double> sp_1{ &values[0], 3 };
+  std::span<double> sp_2{ &values[3], 4 };
+  std::span<double> sp_3{ &values[7], 3 };
+
+  auto ftor = [&](std::size_t i) { return values[i]; };
+  {
+    T blk{};
+    REQUIRE(blk.get_number_of_values() == 0);
+    REQUIRE(!std::isfinite(blk.get_max()));
+    REQUIRE(!std::isfinite(blk.get_min()));
+    REQUIRE(std::isnan(blk.get_mean()));
+    REQUIRE(std::isnan(blk.get_variance()));
+    REQUIRE(std::isnan(blk.get_standard_deviation()));
+  }
+  {
+    T blk{ values[0] };
+    REQUIRE(blk.get_number_of_values() == 1);
+    REQUIRE(blk.get_max() == Approx(0.0));
+    REQUIRE(blk.get_min() == Approx(0.0));
+    REQUIRE(blk.get_mean() == Approx(0.0));
+    REQUIRE(std::isnan(blk.get_variance()));
+    REQUIRE(std::isnan(blk.get_standard_deviation()));
+  }
+  {
+    T blk{ sp_1 };
+    REQUIRE(blk.get_number_of_values() == 3);
+    REQUIRE(blk.get_max() == Approx(2.0));
+    REQUIRE(blk.get_min() == Approx(0.0));
+    REQUIRE(blk.get_mean() == Approx(1.0));
+    REQUIRE(blk.get_variance() == Approx(1.0));
+    REQUIRE(blk.get_standard_deviation() == Approx(1.0));
+  }
+  {
+    T blk{ ftor, 3 };
+    REQUIRE(blk.get_number_of_values() == 3);
+    REQUIRE(blk.get_max() == Approx(2.0));
+    REQUIRE(blk.get_min() == Approx(0.0));
+    REQUIRE(blk.get_mean() == Approx(1.0));
+    REQUIRE(blk.get_variance() == Approx(1.0));
+    REQUIRE(blk.get_standard_deviation() == Approx(1.0));
+  }
+  {
+    T blk{ values };
+    REQUIRE(blk.get_number_of_values() == 10);
+    REQUIRE(blk.get_max() == Approx(9.0));
+    REQUIRE(blk.get_min() == Approx(0.0));
+    REQUIRE(blk.get_mean() == Approx(4.5));
+    REQUIRE(blk.get_variance() == Approx(9.0 + 5.0 / 30.0));
+    REQUIRE(blk.get_standard_deviation() == Approx(std::sqrt(9.0 + 5.0 / 30.0)));
+  }
+}
+
+TEST_CASE()
+{
+  using T = exmath::statistics::max_min_mean_value_calculator_t;
+
+  double values[10] = {
+    0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0,
+  };
+  std::span<double> sp_1{ &values[0], 3 };
+  std::span<double> sp_2{ &values[3], 4 };
+  std::span<double> sp_3{ &values[7], 3 };
+
+  auto ftor = [&](std::size_t i) { return values[i]; };
+  {
+    T blk{};
+
+    for (std::size_t i = 0; i < 3; i++)
+      blk(values[i]);
+
+    REQUIRE(blk.get_number_of_values() == 3);
+    REQUIRE(blk.get_max() == Approx(2.0));
+    REQUIRE(blk.get_min() == Approx(0.0));
+    REQUIRE(blk.get_mean() == Approx(1.0));
+    REQUIRE(blk.get_variance() == Approx(1.0));
+    REQUIRE(blk.get_standard_deviation() == Approx(1.0));
+  }
+  {
+    T blk{};
+
+    blk(sp_1);
+    REQUIRE(blk.get_number_of_values() == 3);
+    REQUIRE(blk.get_max() == Approx(2.0));
+    REQUIRE(blk.get_min() == Approx(0.0));
+    REQUIRE(blk.get_mean() == Approx(1.0));
+    REQUIRE(blk.get_variance() == Approx(1.0));
+    REQUIRE(blk.get_standard_deviation() == Approx(1.0));
+  }
+  {
+    T blk{};
+
+    blk(ftor, 3);
+    REQUIRE(blk.get_number_of_values() == 3);
+    REQUIRE(blk.get_max() == Approx(2.0));
+    REQUIRE(blk.get_min() == Approx(0.0));
+    REQUIRE(blk.get_mean() == Approx(1.0));
+    REQUIRE(blk.get_variance() == Approx(1.0));
+    REQUIRE(blk.get_standard_deviation() == Approx(1.0));
+  }
+  {
+    T blk;
+    blk(sp_1);
+    blk(sp_2);
+    blk(sp_3);
+    REQUIRE(blk.get_number_of_values() == 10);
+    REQUIRE(blk.get_max() == Approx(9.0));
+    REQUIRE(blk.get_min() == Approx(0.0));
+    REQUIRE(blk.get_mean() == Approx(4.5));
+    REQUIRE(blk.get_variance() == Approx(9.0 + 5.0 / 30.0));
+    REQUIRE(blk.get_standard_deviation() == Approx(std::sqrt(9.0 + 5.0 / 30.0)));
+  }
+  {
+    T       blk;
+    T const blk_1(sp_1);
+    T const blk_2(sp_2);
+    T const blk_3(sp_3);
+
+    blk(blk_1);
+    blk(blk_2);
+    blk(blk_3);
+    REQUIRE(blk.get_number_of_values() == 10);
+    REQUIRE(blk.get_max() == Approx(9.0));
+    REQUIRE(blk.get_min() == Approx(0.0));
+    REQUIRE(blk.get_mean() == Approx(4.5));
+    REQUIRE(blk.get_variance() == Approx(9.0 + 5.0 / 30.0));
+    REQUIRE(blk.get_standard_deviation() == Approx(std::sqrt(9.0 + 5.0 / 30.0)));
+  }
+  {
+    T const blk_1(sp_1);
+    T const blk_2(sp_2);
+    T const blk_3(sp_3);
+
+    T blk;
+    blk += blk_1;
+    blk += blk_2;
+    blk += blk_3;
+    REQUIRE(blk.get_number_of_values() == 10);
+    REQUIRE(blk.get_max() == Approx(9.0));
+    REQUIRE(blk.get_min() == Approx(0.0));
+    REQUIRE(blk.get_mean() == Approx(4.5));
+    REQUIRE(blk.get_variance() == Approx(9.0 + 5.0 / 30.0));
+    REQUIRE(blk.get_standard_deviation() == Approx(std::sqrt(9.0 + 5.0 / 30.0)));
+  }
+  {
+    T const blk_1(sp_1);
+    T const blk_2(sp_2);
+    T const blk_3(sp_3);
+
+    T blk = blk_1 + blk_2 + blk_3;
+    REQUIRE(blk.get_number_of_values() == 10);
+    REQUIRE(blk.get_max() == Approx(9.0));
+    REQUIRE(blk.get_min() == Approx(0.0));
+    REQUIRE(blk.get_mean() == Approx(4.5));
+    REQUIRE(blk.get_variance() == Approx(9.0 + 5.0 / 30.0));
+    REQUIRE(blk.get_standard_deviation() == Approx(std::sqrt(9.0 + 5.0 / 30.0)));
+  }
+}
+
+TEST_CASE()
+{
+  using T = exmath::statistics::max_min_mean_value_calculator_t;
+
+  T sig_a{ test_data::signal_a::values };
+  REQUIRE(sig_a.get_max() == Approx(test_data::signal_a::max));
+  REQUIRE(sig_a.get_min() == Approx(test_data::signal_a::min));
+  REQUIRE(sig_a.get_mean() == Approx(test_data::signal_a::mean));
+  REQUIRE(sig_a.get_variance() == Approx(test_data::signal_a::var));
+  REQUIRE(sig_a.get_standard_deviation() == Approx(test_data::signal_a::std));
+
+
+  T sig_b;
+  sig_b(test_data::signal_b::values);
+  REQUIRE(sig_b.get_max() == Approx(test_data::signal_b::max));
+  REQUIRE(sig_b.get_min() == Approx(test_data::signal_b::min));
+  REQUIRE(sig_b.get_mean() == Approx(test_data::signal_b::mean));
+  REQUIRE(sig_b.get_variance() == Approx(test_data::signal_b::var));
+  REQUIRE(sig_b.get_standard_deviation() == Approx(test_data::signal_b::std));
+
+  auto sig_full = sig_a + sig_b;
+  REQUIRE(sig_full.get_max() == Approx(test_data::max));
+  REQUIRE(sig_full.get_min() == Approx(test_data::min));
+  REQUIRE(sig_full.get_mean() == Approx(test_data::mean));
+  REQUIRE(sig_full.get_variance() == Approx(test_data::var));
+  REQUIRE(sig_full.get_standard_deviation() == Approx(test_data::std));
 }
